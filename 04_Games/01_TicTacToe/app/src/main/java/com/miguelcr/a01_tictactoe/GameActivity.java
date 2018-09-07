e6571e6891fa315744d236a3687e49a87aa7c0c8
Miguel CR <camposmiguel@gmail.com>
2018-09-07 15:49:50 +0200

    TicTactToe v2

--- 04_Games/01_TicTacToe/app/src/main/java/com/miguelcr/a01_tictactoe/GameActivity.java
+++ 04_Games/01_TicTacToe/app/src/main/java/com/miguelcr/a01_tictactoe/GameActivity.java
@@ -64,6 +64,7 @@ public class GameActivity extends AppCompatActivity {
 
         // If the cell is empty (is equal 0)
         if(selectedCells[positionClicked] == 0 ) {
+            // 1. We're selecting the cell clicked
             if (isPlayingPlayer1) {
                 ivToChange.setImageResource(R.drawable.ic_player_one);
                 isPlayingPlayer1 = false;
@@ -73,9 +74,69 @@ public class GameActivity extends AppCompatActivity {
                 isPlayingPlayer1 = true;
                 selectedCells[positionClicked] = 2;
             }
+
+            // 2. Check if exist a solution
+            checkWinner();
+
+
         } else {
             Toast.makeText(this, "Try with other cell", Toast.LENGTH_SHORT).show();
         }
+
+    }
+
+    private void checkWinner() {
+        /*
+
+            This is our matrix game
+
+            0 | 1 | 2
+            ----------
+            3 | 4 | 5
+            ----------
+            6 | 7 | 8
+
+            We have 8 different solutions:
+
+            1. 0,1,2
+            2. 3,4,5
+            3. 6,7,8
+            4. 0,3,6
+            5. 1,4,7
+            6. 2,5,8
+            7. 0,4,8
+            8. 2,4,6
+
+         */
+
+        boolean existSolution = false;
+
+        if(selectedCells[0] == selectedCells[1]
+                && selectedCells[0] == selectedCells[2]
+                && selectedCells[0] != 0) {
+            // 1. 0,1,2
+            existSolution = true;
+
+        } else if(selectedCells[3] == selectedCells[4]
+                && selectedCells[3] == selectedCells[5]
+                && selectedCells[3] != 0) {
+            //2. 3,4,5
+            existSolution = true;
+        } else if(selectedCells[6] == selectedCells[7]
+                && selectedCells[6] == selectedCells[8]
+                && selectedCells[6] != 0) {
+            //3. 6,7,8
+            existSolution = true;
+        }
+
+        /* Continue...
+        4. 0,3,6
+        5. 1,4,7
+        6. 2,5,8
+        7. 0,4,8
+        8. 2,4,6
+
+        */
 
     }
 }
